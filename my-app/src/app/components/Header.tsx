04478039979399fd0cc87a087084dbbd5539c290
anastasia <anastasia.ethno@gmail.com>
2023-06-23 19:15:34 +0200

    feat: added cart icon and created svg component

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-/*import '../styles/global.css';*/
 import '../styles/header.css';
+import { CartIcon } from '../components/MySVGComponent'
 
 interface HeaderProps {
   title: string;
@@ -8,8 +8,11 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <header className="header">
-      <h1>{title}</h1>
+    <header className='header'>
+      <h1 className='header_title'>{title}</h1>
+      <div className='header_icon'>
+        <CartIcon id='svg' />
+      </div>
     </header>
   );
 };
