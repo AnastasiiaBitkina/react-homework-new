7752132d7116ea5d116c1da9eb973ed51d230ae9
anastasia <anastasia.ethno@gmail.com>
2023-06-23 21:11:14 +0200

    feat: added filter part

--- my-app/src/app/components/Filter.tsx
+++ my-app/src/app/components/Filter.tsx
@@ -4,9 +4,22 @@ import "../styles/filter.css";
 const Filter: React.FC = () => {
   return (
     <div className="filter">
-
+      <label className="filter-label">Фильтр поиска</label>
+      <div className="input-item">
+        <label className="input-name">Название</label>
+        <input className="input" />
+      </div>
+      <div className="select-genre-item">
+      <label className="genre-name">Жанр</label>
+        <select className="select-genre"></select>
+      </div>
+      <div className="select-cinema-item">
+        <label className="cinema-name">Кинотеатр</label>
+        <select className="select-cinema"></select>
+      </div>
     </div>
   );
 };
 
 export default Filter;
+
