a84c1928cf3a51c936ef7ed35b3f6fd97de0830c
anastasia <anastasia.ethno@gmail.com>
2023-06-26 11:16:58 +0200

    feat: added movie cards

--- my-app/src/app/components/MovieCard.tsx
+++ my-app/src/app/components/MovieCard.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { movies } from '../../../../simple_api/api/mock';
+import Image from 'next/image';
 import "../styles/moviecard.css";
 
 const MovieCard: React.FC = () => {
   return (
     <div className="movie-card">
-
+      {movies.map((movie, index) => (
+        <div className="movie-card-item" key={index}>
+          <div className="movie-pic"><Image
+            src={movie.posterUrl}
+            alt={movie.title}
+            width={100}
+            height={120}
+          />
+          </div>
+          <div className="movie-info">
+            <div className="movie-title">{movie.title}</div>
+            <div className="movie-genre">{movie.genre}</div>
+          </div>
+          <div className="ticket-count"></div>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ my-app/src/app/movie/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/moviecard.css";
+
+const MovieCard: React.FC = () => {
+  return (
+    <div className="movie-card">
+
+    </div>
+  );
+};
+
+export default MovieCard;
