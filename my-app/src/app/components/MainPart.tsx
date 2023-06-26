4b34ce4af3de75974ea983cf2070ce382bf34d7b
anastasia <anastasia.ethno@gmail.com>
2023-06-26 21:49:59 +0200

    feat: added filter by title

--- my-app/src/app/components/MainPart.tsx
+++ my-app/src/app/components/MainPart.tsx
@@ -11,16 +11,26 @@ const MainPart: React.FC = () => {
 
   const [selectedCinema, setSelectedCinema] = useState("");
 
+  const [selectedMovieTitle, setSelectedMovieTitle] = useState("");
+
   const handleCinemaChange = (cinema: string) => {
     setSelectedCinema(cinema);
   };
 
+  const handleMovieTitleChange = (title: string) => {
+    setSelectedMovieTitle(title);
+  }
+  
+  
+
   return (
     <div className="main-part">
       <Filter onGenreChange={setSelectedGenre}
-        onCinemaChange={handleCinemaChange} />
+        onCinemaChange={handleCinemaChange}
+        onMovieTitleChange={handleMovieTitleChange} />
       <MovieCard selectedGenre={selectedGenre}
-      selectedCinema={selectedCinema}  />
+      selectedCinema={selectedCinema}
+      selectedMovieTitle={selectedMovieTitle}  />
     </div>
   );
 };
