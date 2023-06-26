d2b7e6eaaa1a78335de8c1de32ab4a90d2dfc8ab
anastasia <anastasia.ethno@gmail.com>
2023-06-26 11:48:10 +0200

    feat: added select genre

--- my-app/src/app/components/Filter.tsx
+++ my-app/src/app/components/Filter.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+'use client'
+
+import React, { useState, useEffect } from "react";
+import { movies } from "../../../../simple_api/api/mock";
 import "../styles/filter.css";
 
 const Filter: React.FC = () => {
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  const uniqueGenres = Array.from(new Set(movies.map((movie) => movie.genre)));
+
+  useEffect(()=>{
+    setSelectedGenre("");
+  },[]);
   return (
     <div className="filter">
       <label className="filter-label">Фильтр поиска</label>
@@ -11,7 +21,19 @@ const Filter: React.FC = () => {
       </div>
       <div className="select-genre-item">
       <label className="genre-name">Жанр</label>
-        <select className="select-genre"></select>
+        <select 
+        className="select-genre"
+        value={selectedGenre}
+        onChange={(e) => setSelectedGenre(e.target.value)}>
+          <option 
+          className="select-default-genre"
+          value="">Выберите жанр</option>
+        {uniqueGenres.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="select-cinema-item">
         <label className="cinema-name">Кинотеатр</label>
