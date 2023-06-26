2924443316f0331bc2251c9effef083148748b4b
anastasia <anastasia.ethno@gmail.com>
2023-06-26 12:31:28 +0200

    fix: fixed filter by genre

--- my-app/src/app/components/Filter.tsx
+++ my-app/src/app/components/Filter.tsx
@@ -4,14 +4,15 @@ import React, { useState, useEffect } from "react";
 import { movies } from "../../../../simple_api/api/mock";
 import "../styles/filter.css";
 
-const Filter: React.FC = () => {
-  const [selectedGenre, setSelectedGenre] = useState("");
 
+const Filter: React.FC<{ onGenreChange: (genre: string) => void }>= ({ onGenreChange }) => {
   const uniqueGenres = Array.from(new Set(movies.map((movie) => movie.genre)));
 
-  useEffect(()=>{
-    setSelectedGenre("");
-  },[]);
+  const handleGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedGenre = e.target.value;
+    onGenreChange(selectedGenre);
+  };
+
   return (
     <div className="filter">
       <label className="filter-label">Фильтр поиска</label>
@@ -21,10 +22,7 @@ const Filter: React.FC = () => {
       </div>
       <div className="select-genre-item">
       <label className="genre-name">Жанр</label>
-        <select 
-        className="select-genre"
-        value={selectedGenre}
-        onChange={(e) => setSelectedGenre(e.target.value)}>
+        <select className="select-genre" onChange={handleGenreChange}>
           <option 
           className="select-default-genre"
           value="">Выберите жанр</option>
--- my-app/src/app/components/MainPart.tsx
+++ my-app/src/app/components/MainPart.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import "../styles/main.css";
 import Filter from "../components/Filter"
 import MovieCard from "./MovieCard";
 
 const MainPart: React.FC = () => {
+  const [selectedGenre, setSelectedGenre] = useState("");
   return (
     <div className="main-part">
-        <Filter />
-        <MovieCard />
+        <Filter onGenreChange={setSelectedGenre}  />
+        <MovieCard selectedGenre={selectedGenre} />
     </div>
   );
 };
