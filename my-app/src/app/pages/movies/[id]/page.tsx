5822f510c686848e10b11e3a0c84d215e69faaa8
anastasia <anastasia.ethno@gmail.com>
2023-06-27 03:01:27 +0200

    feat: added card page

--- my-app/src/app/pages/movies/[id]/page.tsx
+++ my-app/src/app/pages/movies/[id]/page.tsx
@@ -2,67 +2,47 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import { movies } from "../../../../../../simple_api/api/mock";
+import { movies, reviews } from "../../../../../../simple_api/api/mock";
 import Image from "next/image";
+import Review from "../../../components/review"
+import TicketCount from "../../../components/TicketCount";
 import "../../../styles/moviepage.css";
 
-export default function MoviePage({ params }: { params: { id: string } }) {
+
+
+export default function MoviePage({ params }: { params: { id: string } })  {
   const movie = movies.find((movie) => movie.id === params.id);
   if (!movie) {
-    return <div>Loading...</div>;
+    return <div className="loading">Loading...</div>;
   }
-
-
-    return (
+  
+  return (
+    <div className="movie-page">
       <div className="movie-card">
-        <div className="movie-pic">
+        <div>
           <Image
             src={movie.posterUrl}
             alt={movie.title}
-            width={100}
-            height={120}
+            width={400}
+            height={500}
+            className="movie-pic"
           />
         </div>
         <div className="movie-info">
           <div className="movie-title">{movie.title}</div>
-          <div className="movie-genre">{movie.genre}</div>
-          <div className="movie-description">{movie.description}</div>
+          <div className="movie-genre">Жанр:<div className="movie-genre-content"> {movie.genre}</div></div>
+          <div className="movie-year">Год выпуска:<div className="movie-year-content">{movie.releaseYear}</div></div>
+          <div className="movie-rating">Рейтинг: <div className="movie-rating-content">{movie.rating}</div></div>
+          <div className="movie-director">Режиссер: <div className="movie-director-content">{movie.director}</div></div>
+          <div className="movie-description">Описание</div>
+          <div className="movie-description-content">{movie.description}</div>
+        </div>
+        <div className="ticket-count">
         </div>
       </div>
-    );
-}
-
-/*const MoviePage: React.FC = () => {
- const router = useRouter();
-
-  const { id } = router.query as { id: string };
-
-  const movie = movies.find((movie) => movie.id === id);
-
-  /*const movie = movies.find((movie) => movie.id === "2aT976Fs_Bek0e2ZR_05V");*/
-
-  /*if (!movie) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="movie-card">
-      <div className="movie-pic">
-        <Image
-          src={movie.posterUrl}
-          alt={movie.title}
-          width={100}
-          height={120}
-        />
-      </div>
-      <div className="movie-info">
-        <div className="movie-title">{movie.title}</div>
-        <div className="movie-genre">{movie.genre}</div>
-        <div className="movie-description">{movie.description}</div>
-      </div>
+      <Review params={params} />
     </div>
   );
-};
+} 
 
-export default MoviePage;*/
 
